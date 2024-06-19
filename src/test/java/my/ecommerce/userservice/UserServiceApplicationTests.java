65869937fc604c5f490e1eb7ad154e4fe91e036b
ewhdtls0 <ewhdtls0@naver.com>
2024-06-19 22:11:48 +0900

    FEAT: user-service 애플리케이션 유레카 등록

---
+++ src/test/java/my/ecommerce/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.ecommerce.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
