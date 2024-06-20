e25f59d847104d33e0b59c96ad575cc2679cb3da
ewhdtls0 <ewhdtls0@naver.com>
2024-06-20 16:04:56 +0900

    FEAT: user-service 회원가입

--- src/main/java/my/ecommerce/userservice/controller/UserController.java
+++ src/main/java/my/ecommerce/userservice/controller/UserController.java
@@ -1,25 +1,26 @@
 package my.ecommerce.userservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import my.ecommerce.userservice.dto.UserDto;
+import my.ecommerce.userservice.service.UserService;
 import my.ecommerce.userservice.vo.Greeting;
-import org.springframework.beans.factory.annotation.Autowired;
+import my.ecommerce.userservice.vo.RequestUser;
+import my.ecommerce.userservice.vo.ResponseUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
 
-    private Environment env;
-
-    @Autowired
-    private Greeting greeting;
-
-    @Autowired
-    public UserController(Environment env) {
-        this.env = env;
-    }
+    private final Environment env;
+    private final Greeting greeting;
+    private final UserService userService;
 
     @GetMapping("/health_check")
     public String status() {
@@ -30,5 +31,17 @@ public class UserController {
     public String weclome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 }
--- src/main/java/my/ecommerce/userservice/service/UserServiceImpl.java
+++ src/main/java/my/ecommerce/userservice/service/UserServiceImpl.java
@@ -25,6 +25,7 @@ public class UserServiceImpl implements UserService{
         userEntity.setEncryptedPwd("encrypted_password");
 
         userRepository.save(userEntity);
-        return null;
+
+        return mapper.map(userEntity, UserDto.class);
     }
 }
